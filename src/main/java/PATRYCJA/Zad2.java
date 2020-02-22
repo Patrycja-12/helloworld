e5a6fe6bd3367000eae7605592ed21f0b77daa2c
Patrycja <patzyc9@gmail.com>
2020-02-22 12:13:34 +0100

    4

---
+++ src/main/java/PATRYCJA/Zad2.java
@@ -0,0 +1,9 @@
+package PATRYCJA;
+
+public class Zad2 {
+    public String PRAWO="P";
+    public String LEWO="L";
+    public String GÓRA="G";
+    public String DÓŁ="D";
+    public String ZAKOŃCZ="Z";
+}
