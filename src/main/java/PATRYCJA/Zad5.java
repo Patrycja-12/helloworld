8431ec33ba773f7167dfcef4fec3b3bc3a39ad68
Patrycja <patzyc9@gmail.com>
2020-02-22 14:02:50 +0100

    4

---
+++ src/main/java/PATRYCJA/Zad5.java
@@ -0,0 +1,21 @@
+package PATRYCJA;
+
+public class Zad5 {
+    private Zad5 left;
+    private Zad5 right;
+
+    public Zad5 invertTree(Zad5 root) {
+        if (root == null) {
+            return root;
+            invertTree(root.left);
+            invertTree(root.right);
+            Zad5 t = root.left;
+            root.left = root.right;
+            root.right = t;
+            return root;
+        }
+    }
+}
+
+
+
