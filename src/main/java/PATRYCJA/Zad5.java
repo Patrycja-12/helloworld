8c947439c49623c50d654db44935317c48d06640
Patrycja <patzyc9@gmail.com>
2020-02-22 14:18:19 +0100

    4

--- src/main/java/PATRYCJA/Zad5.java
+++ src/main/java/PATRYCJA/Zad5.java
@@ -4,11 +4,11 @@ public class Zad5 {
     private Zad5 left;
     private Zad5 right;
 
-    public Zad5 invertTree(Zad5 root) {
+    public Zad5 OdwracanieDrzewa(Zad5 root) {
         if (root == null) {
             return root;
-            invertTree(root.left);
-            invertTree(root.right);
+            OdwracanieDrzewa(root.left);
+            OdwracanieDrzewa(root.right);
             Zad5 t = root.left;
             root.left = root.right;
             root.right = t;
